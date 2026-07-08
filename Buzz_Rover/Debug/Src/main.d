94f3f99c265d88f4d0198495d08ae3baee40595b
justyoonjh <juhoyoon0901@naver.com>
2026-07-09 02:59:37 +0900

    Basic_Timer-5(TIM Interrupt&LED toggle) - buzz_tim.h/.c TIM_GetOverflowCount, TIM_GetTimeStamp, TIM_GetElapsedUS_Long -> count overflow cycle - main.c NVIC_ISER(0) |= (1 << 28);
    Long Elapsed : 16333 us

--- Buzz_Rover/Debug/Src/main.d
+++ Buzz_Rover/Debug/Src/main.d
@@ -8,9 +8,6 @@ Src/main.o: ../Src/main.c \
  /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/bsp/stm32f446xx_tim.h \
  /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/drivers/Inc/buzz_rcc.h \
  /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/drivers/Inc/buzz_systick.h \
- /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/drivers/Inc/buzz_i2c.h \
- /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/drivers/Inc/buzz_usart.h \
- /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/app/Inc/mpu6050.h \
  /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/drivers/Inc/buzz_tim.h \
  /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/debug_app/Inc/tim_profiling.h
 /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/drivers/Inc/buzz_gpio.h:
@@ -22,8 +19,5 @@ Src/main.o: ../Src/main.c \
 /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/bsp/stm32f446xx_tim.h:
 /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/drivers/Inc/buzz_rcc.h:
 /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/drivers/Inc/buzz_systick.h:
-/home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/drivers/Inc/buzz_i2c.h:
-/home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/drivers/Inc/buzz_usart.h:
-/home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/app/Inc/mpu6050.h:
 /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/drivers/Inc/buzz_tim.h:
 /home/justjhst/바탕화면/self-balancing-robot/Buzz_Rover/debug_app/Inc/tim_profiling.h:
