8dad91af84da2d7c9a3b5cdd2ccae93ec6f4e496
justyoonjh <juhoyoon0901@naver.com>
2026-07-18 01:53:33 +0900

    Timer_PWM(Edge-aligned Mode) practice(error) led isn't run

--- Buzz_Rover/drivers/Inc/buzz_tim.h
+++ Buzz_Rover/drivers/Inc/buzz_tim.h
@@ -5,6 +5,20 @@
 
 typedef struct
 {
+	uint8_t TIM_Channel;
+	uint16_t TIM_Prescaler;
+	uint16_t TIM_Period;
+	uint8_t TIM_OCPreload;
+}TIM_Config_t;
+
+typedef struct
+{
+	TIM_RegMap_t *pTIMx;
+	TIM_Config_t TIM_Config;
+}TIM_Handle_t;
+
+typedef struct
+{
 	uint32_t overflow_count;
 	uint32_t cnt;
 }TIM_Timestamp_t;
@@ -17,5 +31,7 @@ void TIM_TimeBase_Init(TIM_RegMap_t *pTIMx, uint16_t psc, uint32_t arr);
 void TIM_Start(TIM_RegMap_t *pTIMx);
 
 uint32_t TIM_GetElapsedUs(TIM_RegMap_t *pTIMx, uint32_t start_cnt);
+
+void TIM_PWM_Init(TIM_Handle_t *pTIMHandle);
 
 #endif
